2ae5ce7500f2640733a8253df372afede96d2ae9
Ambre Bidault <ambrebidault@orange.fr>
2023-07-12 20:12:22 +0200

    correctifs visuels, réorganisation de fichiers et ajout numérotation slide dans le carrousel

--- src/components/Slideshow/index.jsx
+++ src/components/Slideshow/index.jsx
@@ -18,11 +18,15 @@ function Slideshow(images) {
         setIndex(newIndex > length -1 ? 0 : newIndex);
         return newIndex
     };
+
+    const imgNum = index + 1;
+    const imgsTotal = images.length; 
     
     return (
         <div className="slideshow">
             <i className={images.length <= 1 ? "fa fa-chevron-left hide" : "fa fa-chevron-left" } onClick={arrowLeft}></i>
             <i className={images.length <= 1? "fa fa-chevron-right hide" : "fa fa-chevron-right" } onClick={arrowRight}></i>
+            <p className="slideshow__count">{imgNum}/{imgsTotal}</p>
             {Slide(index, images)}
         </div>
     )
